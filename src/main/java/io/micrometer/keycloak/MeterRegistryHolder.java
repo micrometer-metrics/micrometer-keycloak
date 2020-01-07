0293d46d9a39d000afae4f1d50bf956ad9995ed7
Jon Schneider <jschneider@gradle.com>
2020-01-07 12:10:34 -0500

    Bind common meter binders to keycloak apps

--- src/main/java/io/micrometer/keycloak/MeterRegistryHolder.java
+++ src/main/java/io/micrometer/keycloak/MeterRegistryHolder.java
@@ -1,6 +1,13 @@
 package io.micrometer.keycloak;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.system.FileDescriptorMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.core.instrument.binder.system.UptimeMetrics;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
@@ -13,6 +20,14 @@ public class MeterRegistryHolder {
     private final static MeterRegistryHolder INSTANCE = new MeterRegistryHolder();
 
     private MeterRegistryHolder() {
+        MeterRegistry meterRegistry = getMeterRegistry();
+        new ProcessorMetrics().bindTo(meterRegistry);
+        new JvmGcMetrics().bindTo(meterRegistry);
+        new JvmMemoryMetrics().bindTo(meterRegistry);
+        new JvmThreadMetrics().bindTo(meterRegistry);
+        new FileDescriptorMetrics().bindTo(meterRegistry);
+        new ClassLoaderMetrics().bindTo(meterRegistry);
+        new UptimeMetrics().bindTo(meterRegistry);
     }
 
     public static MeterRegistryHolder getInstance() {
